0d42bd293e30aab5fab932e0023673d31829b58e
Aleksandar Drakaliyski <aleksandardrakaliyski@Mac.broadband>
2024-11-25 22:32:13 +0000

    Big fixes

--- components/SectionItem.tsx
+++ components/SectionItem.tsx
@@ -10,6 +10,10 @@ import { UserProps } from "@/types/user";
 import ProfileImage from "./ProfileImage";
 import Link from "next/link";
 import { SectionProps } from "@/types/section";
+import { Button } from "./ui/button";
+import axios from "axios";
+import BASE_URL from "@/lib/config";
+import { Trash } from "lucide-react";
 
 const SectionItem = ({
   section,
@@ -18,19 +22,40 @@ const SectionItem = ({
   section: SectionProps;
   user: UserProps;
 }) => {
+  const deleteSection = async () => {
+    axios
+      .delete(`${BASE_URL}/section/${section.id}`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Max-Age", "1800");
+        res.setHeader("Access-Control-Allow-Headers", "content-type");
+        res.setHeader(
+          "Access-Control-Allow-Methods",
+          "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+        );
+      })
+      .catch((error) => console.log("Error: ", error));
+  };
+
   return (
-    <Link href={`/section/${section.id}`}>
-      <Card className="mt-3 mb-4">
-        <CardHeader>
-          <CardTitle>{section.title}</CardTitle>
-          <CardDescription>{section.subtitle}</CardDescription>
-        </CardHeader>
-        <CardFooter className="space-x-3">
-          <ProfileImage user={user} />
-          <p>By {user.name}</p>
-        </CardFooter>
-      </Card>
-    </Link>
+    // <Link href={`/section/${section.id}`}>
+    <Card className="mt-3 mb-4">
+      <CardHeader>
+        <CardTitle>{section.title}</CardTitle>
+        <CardDescription>{section.subtitle}</CardDescription>
+      </CardHeader>
+      <CardFooter className="space-x-3">
+        <ProfileImage user={user} />
+        <p>By {user.name}</p>
+      </CardFooter>
+      <Button onClick={deleteSection}>
+        <Trash />
+      </Button>
+    </Card>
+    // </Link>
   );
 };
 
--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: "http://localhost:8080/:path*", // Fixed the extra slash
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
