566bd0d16bf06691826174176ad279989ba8e030
Aleksandar Drakaliyski <aleksandardrakaliyski@Mac.broadband>
2024-11-25 22:32:19 +0000

    Big fixes

--- components/SectionItem.tsx
+++ components/SectionItem.tsx
@@ -29,14 +29,11 @@ const SectionItem = ({
       })
       .then((res) => {
         res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Credentials", "true");
-        res.setHeader("Access-Control-Max-Age", "1800");
-        res.setHeader("Access-Control-Allow-Headers", "content-type");
-        res.setHeader(
-          "Access-Control-Allow-Methods",
-          "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-        );
-      })
+		res.setHeader("Access-Control-Allow-Credentials", "true");
+		res.setHeader("Access-Control-Max-Age", "1800");
+		res.setHeader("Access-Control-Allow-Headers", "content-type");
+		res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+      }))
       .catch((error) => console.log("Error: ", error));
   };
 
