2f134d046305abbdec836a50911d67b9912275f4
Aleksandar Drakaliyski <aleksandardrakaliyski@Mac.broadband>
2024-12-04 10:49:03 +0000

    Added searchbox

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import SectionItem from "@/components/SectionItem";
 import { SkeletonCard } from "@/components/SkeletonCard";
 import UserInfo from "@/components/UserInfo";
 import BASE_URL from "@/lib/config";
+import { SectionProps } from "@/types/section";
 import { UserProps } from "@/types/user";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
@@ -14,24 +16,54 @@ const Profile = () => {
     avatar_url: "",
     name: "",
   });
+  const [sections, setSections] = useState<SectionProps[]>([]);
+
   const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(true);
-    axios
-      .get(`${BASE_URL}/user`, {
-        withCredentials: true,
-      })
-      .then((res) => setUser(res.data))
-      .catch((error) => console.log("Error: ", error))
-      .finally(() => setLoading(false));
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const [userRes, sectionsRes] = await Promise.all([
+          axios.get(`${BASE_URL}/user`, { withCredentials: true }),
+          axios.get(`${BASE_URL}/section`, {
+            withCredentials: true,
+          }),
+        ]);
+
+        setUser(userRes.data);
+        setSections(sectionsRes.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
   }, []);
 
+  const fetchSections = async () => {
+    axios
+      .get(`${BASE_URL}/section`, {
+        withCredentials: true,
+      })
+      .then((res) => setSections(res.data))
+      .catch((e) => console.log(e));
+  };
+
   return (
-    <div className="container">
+    <div className="ml-[20px] mr-[20px] sm:ml-[30px] sm:mr-[30px] md:ml-[100px] md:mr-[100px] lg:ml-[200px] lg:mr-[200px] xl:ml-[300px] xl:mr-[300px]">
       <h1 className="heading">Profile</h1>
       {isLoading ? <SkeletonCard /> : <UserInfo user={user!} />}
       <h1 className="heading">Recent Sections</h1>
+      {sections.map((section, id: number) => (
+        <SectionItem
+          key={id}
+          section={section}
+          user={user}
+          refreshSection={fetchSections}
+        />
+      ))}
     </div>
   );
 };
--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import SectionItem from "@/components/SectionItem";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
@@ -7,11 +6,14 @@ import BASE_URL from "../lib/config";
 import { UserProps } from "@/types/user";
 import { SkeletonCard } from "@/components/SkeletonCard";
 import { SectionProps } from "@/types/section";
+import Searchbox from "@/components/Searchbox";
 
 const HomePage = () => {
   const [user, setUser] = useState<UserProps>();
   const [sections, setSections] = useState<SectionProps[]>([]);
+  const [filteredSections, setFilteredSections] = useState<SectionProps[]>([]);
   const [isLoading, setLoading] = useState(true);
+  const [searchQuery, setSearchQuery] = useState<string>("");
 
   const fetchData = async () => {
     setLoading(true);
@@ -29,6 +31,7 @@ const HomePage = () => {
 
       setUser(userRes.data);
       setSections(sectionsRes.data);
+      setFilteredSections(sectionsRes.data);
     } catch (error) {
       console.error("Error fetching data:", error);
     } finally {
@@ -40,11 +43,30 @@ const HomePage = () => {
     fetchData();
   }, []);
 
-  if (user === undefined) return <div className="heading">Please login</div>;
+  useEffect(() => {
+    if (searchQuery === "") {
+      setFilteredSections(sections);
+    } else {
+      setFilteredSections(
+        sections.filter((section) =>
+          section.title.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+      );
+    }
+  }, [searchQuery, sections]);
+
+  if (user === undefined)
+    return (
+      <div className="items-center">
+        <h1 className="text-9xl">Unauthenticated</h1>
+        <p className="text-3xl">Please log in</p>
+      </div>
+    );
 
   return (
     <div className="heading space-y-3">
-      <h1 className="">Home</h1>
+      <h1>Home</h1>
+      <Searchbox onSearch={setSearchQuery} />
       {isLoading ? (
         <>
           {[...Array(10)].map((_, index) => (
@@ -52,7 +74,7 @@ const HomePage = () => {
           ))}
         </>
       ) : (
-        sections.map((section: SectionProps, id: number) => (
+        filteredSections.map((section: SectionProps, id: number) => (
           <SectionItem
             key={id}
             section={section}
@@ -61,6 +83,7 @@ const HomePage = () => {
           />
         ))
       )}
+      {filteredSections.length === 0 && <p>No sections found</p>}
     </div>
   );
 };
